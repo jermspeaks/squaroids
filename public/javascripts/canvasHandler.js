c7f92aeae2b4e1735058e85628e8a7413b1acc8a
LendelTheGreat <langer.tomas@yahoo.com>
2014-10-25 22:44:44 +0100

    debug basic rendering

--- public/javascripts/canvasHandler.js
+++ public/javascripts/canvasHandler.js
@@ -3,23 +3,26 @@ $(document).ready(function(){
   ctx = canvas.getContext('2d');
   CANVAS_WIDTH = canvas.width;
   CANVAS_HEIGHT = canvas.height;
-  initialise();
   
   //temporary data to be fetched from models
   earth_size = 200;
   ast_size = 50;
+  
+  initialise();
 })
 
-initialise(){
+function initialise(){
   //stroke around the whole canvas
   ctx.strokeStyle = 'black';
   ctx.strokeRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   
   //draw earth
   ctx.fillStyle = 'navy';
-  ctx.fillRect((CANVAS_WIDTH - earth_size)/2, CANVAS_HEIGHT* 3/4, earth_size, earth_size);
+  ctx.fillRect((CANVAS_WIDTH - earth_size)/2, CANVAS_HEIGHT - earth_size, earth_size, earth_size);
   
-  //draw temp asteroid
+  //draw temp asteroids
   ctx.fillStyle = 'red';
-  ctx.fillRect(0, 0, ast_size, ast_size);
+  ctx.fillRect(50, 50, ast_size, ast_size);
+  ctx.fillRect(150, 150, ast_size, ast_size);
+  ctx.fillRect(250, 250, ast_size, ast_size);
 }

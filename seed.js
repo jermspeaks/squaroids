9ab37883b3b792e72ad482e7e0b62e7e284efde7
jermspeaks <jermspeaks@gmail.com>
2014-10-25 15:28:20 -0700

    seed file now added

---
+++ seed.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+var AsteroidModel = require('./models/Asteroids.js').Asteroid
+mongoose.connect('mongodb://localhost/asteroids')
+var dir = './seeds';
+var db = mongoose.connection;
+
+// Show connection error if there is one
+db.on('error', console.error.bind(console, 'Database Connection Error:'));
+
+// If we successfully connected to mongo
+db.once('open', function callback() {
+
+  var fs = require('fs');
+
+  fs.readdir(dir, function(err, list){
+    if(err) {
+      console.log('Error: ' + err);
+    }
+
+    list.forEach(function(file){
+      var collection_name = file.split(".")[0];
+      console.log(collection_name);
+      var parsedJSON = require(dir + '/' + file);
+
+      for(var i = 0; i < parsedJSON.length; i++) {
+        AsteroidModel.create(parsedJSON[i]);
+        console.log('Item: ' + i + ' has been recorded');
+        // db.collection(collection_name).insert(parsedJSON[i], function(err, records){
+        // })
+      }
+
+    });
+  });
+
+});
+
+mongoose.disconnect();
+
